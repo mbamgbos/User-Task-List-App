5c911b01301b9e6f6e2620792d8fd7ba18c6f239
Margaret Bamgbose <mbamgbos@nd.edu>
2015-11-04 10:44:39 -0500

    errors with app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :task_lists
+
+  resources :tasks
+
   resources :tests
 
   # The priority is based upon order of creation: first created -> highest priority.

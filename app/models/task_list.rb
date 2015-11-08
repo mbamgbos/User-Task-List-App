aff9714dc07bbd7416e1cd77718bb8dbdb29d2d6
Margaret Bamgbose <mbamgbos@nd.edu>
2015-11-08 17:35:36 -0500

    task app with validations

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -1,7 +1,7 @@
 class TaskList < ActiveRecord::Base
 	has_many :tasks
 	
-	validates :title, :description, presence: {message: "is required" }
+	validates :name, :description, presence: {message: "is required" }
 	
-	validates :title, uniqueness: true
+	validates :name, uniqueness: true
 end

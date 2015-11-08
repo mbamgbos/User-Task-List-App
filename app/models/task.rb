bf14cd7a2086eaa78360ca7686af0bda96a82062
Margaret Bamgbose <mbamgbos@nd.edu>
2015-11-08 16:25:10 -0500

    check for validation error

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,10 @@
 class Task < ActiveRecord::Base
   belongs_to :task_list
+  
+  validates :title, :priority, :due_date, :task_list, presence: { message: "is required" } 
+  
+  validates :priority, numericality: true
+  
+  validates :priority, inclusion: { in: 1..10, message: 'needs to be between 1 and 10' }
+  
 end
--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -1,3 +1,7 @@
 class TaskList < ActiveRecord::Base
 	has_many :tasks
+	
+	validates :title, :description, presence: {message: "is required" }
+	
+	validates :title, uniqueness: true
 end

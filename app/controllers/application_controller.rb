46d7cf9466ac235c4e1fb679975e26c8b0c229f7
Margaret Bamgbose <mbamgbos@nd.edu>
2015-12-02 10:42:32 -0500

    layout app error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+   before-action :get_all_task_lists
+  
+  layout 'todo_list'
+  
+  def get_all_task_lists
+  @all_task_lists=task_lists.all
+  end
 end
